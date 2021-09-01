95af7e83fd74acb5607bd0ae5c631c2431260713
KeisukeShimokawa <shimoke4869@gmail.com>
2021-09-02 02:12:50 +0900

    抽象クラスに変更 #7

--- src/domain/shared/Identifier.ts
+++ src/domain/shared/Identifier.ts
@@ -1,7 +1,10 @@
 import { nanoid } from 'nanoid';
 import { ValueObject } from './ValueObject';
 
-export class Identifier<U extends string> extends ValueObject<string, U> {
+export abstract class Identifier<U extends string> extends ValueObject<
+  string,
+  U
+> {
   constructor(id?: string) {
     super(id ? id : nanoid());
   }
