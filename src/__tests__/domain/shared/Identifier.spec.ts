f00671ca064f3fe3b822348f9d4e961870be321f
KeisukeShimokawa <shimoke4869@gmail.com>
2021-09-01 23:06:51 +0900

    nanoidを使用して識別子を自動採番するテストとモックを追加 #7

--- src/__tests__/domain/shared/Identifier.spec.ts
+++ src/__tests__/domain/shared/Identifier.spec.ts
@@ -5,7 +5,15 @@ class DummyIdentifier extends Identifier<'DummyIdentifier'> {}
 
 describe('識別子 Identifier の基底クラス', () => {
   describe('文字列の識別子を自動生成する', () => {});
-  describe('識別子の等価性を担保できる', () => {});
+  let nanoidSpy: jest.SpyInstance;
+
+  beforeEach(() => {
+    nanoidSpy = jest.spyOn(nanoid, 'nanoid');
+  });
+
+  afterEach(() => {
+    nanoidSpy.mockReset();
+  });
 
   describe('文字列の識別子を引数で与えることができる', () => {
     it('引数に識別子となる文字列 "id-1" を与えて、識別子クラスを生成する', () => {
@@ -14,6 +22,20 @@ describe('識別子 Identifier の基底クラス', () => {
 
       // When
       const actual = new DummyIdentifier(expected);
+
+      // Then
+      expect(actual.value).toBe(expected);
+    });
+  });
+
+  describe('文字列の識別子を自動生成する', () => {
+    it('引数に何も指定しない場合、nanoid を使用して自動的に識別子を生成する', () => {
+      // Given
+      const expected = '1234';
+      nanoidSpy.mockReturnValueOnce(expected);
+
+      // When
+      const actual = new DummyIdentifier();
 
       // Then
       expect(actual.value).toBe(expected);
