c00a767de70f777500b9ca9265f34b759d8657a1
KeisukeShimokawa <shimoke4869@gmail.com>
2021-09-01 23:04:04 +0900

    テスト用の識別子の仮実装を追加する方法を記述記述

--- src/__tests__/domain/shared/Identifier.spec.ts
+++ src/__tests__/domain/shared/Identifier.spec.ts
@@ -1,3 +1,8 @@
+import { Identifier } from 'src/domain/shared/Identifier';
+import * as nanoid from 'nanoid';
+
+class DummyIdentifier extends Identifier<'DummyIdentifier'> {}
+
 describe('識別子 Identifier の基底クラス', () => {
   describe('文字列の識別子を引数で与えることができる', () => {});
   describe('文字列の識別子を自動生成する', () => {});
