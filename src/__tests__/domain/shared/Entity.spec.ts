0a143836a267c586824dd774af0f579641008ac8
KeisukeShimokawa <shimoke4869@gmail.com>
2021-09-02 02:14:11 +0900

    同じPropsを有しており、識別子の値も同じ場合のテストケースを追加 #7

--- src/__tests__/domain/shared/Entity.spec.ts
+++ src/__tests__/domain/shared/Entity.spec.ts
@@ -15,9 +15,29 @@ class DummyEntity extends Entity<DummyEntityProps, DummyIdentifier> {
 }
 
 describe('エンティティ Entity の基底クラス', () => {
-  describe('同一性担保できる', () => {
-    it('同じ値の識別子を自動採番し、属性が同じ場合、同じエンティティだと判断する', () => {});
     it('同じ値の識別子を自動採番し、属性が異なる場合、同じエンティティだと判断する', () => {});
     it('異なる識別子を自動採番し、属性が同じ場合、異なるエンティティだと判断する', () => {});
+  describe('同一性を担保できる', () => {
+    it('同じ値の識別子を自動採番し、属性が同じ場合、同じエンティティだと判断する', () => {
+      // Given
+      const identifier1 = new DummyIdentifier('sample');
+      const props1 = {
+        name: 'sample',
+        age: 1,
+      };
+      const identifier2 = new DummyIdentifier('sample');
+      const props2 = {
+        name: 'sammple',
+        age: 1,
+      };
+
+      // When
+      const entity1 = new DummyEntity(identifier1, props1);
+      const entity2 = new DummyEntity(identifier2, props2);
+
+      // Then
+      expect(entity1.equals(entity2)).toBeTruthy();
+    });
+
   });
 });
