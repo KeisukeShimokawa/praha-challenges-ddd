089092a872938776db927352c0c2868bf8240107
KeisukeShimokawa <shimoke4869@gmail.com>
2021-09-01 23:05:35 +0900

    引数で識別子となる文字列を与えた場合の振る舞いに関するテストを追加 #7

--- src/__tests__/domain/shared/Identifier.spec.ts
+++ src/__tests__/domain/shared/Identifier.spec.ts
@@ -4,7 +4,20 @@ import * as nanoid from 'nanoid';
 class DummyIdentifier extends Identifier<'DummyIdentifier'> {}
 
 describe('識別子 Identifier の基底クラス', () => {
-  describe('文字列の識別子を引数で与えることができる', () => {});
   describe('文字列の識別子を自動生成する', () => {});
   describe('識別子の等価性を担保できる', () => {});
+
+  describe('文字列の識別子を引数で与えることができる', () => {
+    it('引数に識別子となる文字列 "id-1" を与えて、識別子クラスを生成する', () => {
+      // Given
+      const expected = 'id-1';
+
+      // When
+      const actual = new DummyIdentifier(expected);
+
+      // Then
+      expect(actual.value).toBe(expected);
+    });
+  });
+
 });
