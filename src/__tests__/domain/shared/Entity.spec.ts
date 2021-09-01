28a4d78d2f49d15cc0fb9fca4a78ab879e7d5a8f
KeisukeShimokawa <shimoke4869@gmail.com>
2021-09-02 02:08:15 +0900

    エンティティクラスに関するダミー実装を追加 #7

--- src/__tests__/domain/shared/Entity.spec.ts
+++ src/__tests__/domain/shared/Entity.spec.ts
@@ -1,3 +1,19 @@
+import { Entity } from 'src/domain/shared/Entity';
+import { Identifier } from 'src/domain/shared/Identifier';
+
+interface DummyEntityProps {
+  name: string;
+  age: number;
+}
+
+class DummyIdentifier extends Identifier<'DummyIdentifier'> {}
+
+class DummyEntity extends Entity<DummyEntityProps, DummyIdentifier> {
+  constructor(id: DummyIdentifier, props: DummyEntityProps) {
+    super(id, props);
+  }
+}
+
 describe('エンティティ Entity の基底クラス', () => {
   describe('同一性担保できる', () => {
     it('同じ値の識別子を自動採番し、属性が同じ場合、同じエンティティだと判断する', () => {});
