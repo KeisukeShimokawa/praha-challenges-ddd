eb7058d3550e91d4e3b65f2a04f290e9a72772f0
KeisukeShimokawa <shimoke4869@gmail.com>
2021-09-05 16:49:56 +0900

    テストコードを追加 #1

--- src/__tests__/domain/participants/ParticipantEmail.spec.ts
+++ src/__tests__/domain/participants/ParticipantEmail.spec.ts
@@ -1,10 +1,26 @@
+import { ParticipantEmail } from 'src/domain/participants/ParticipantEmail';
+
 describe('値オブジェクト<"参加者のメールアドレス"> ParticipantEmail', () => {
   describe('参加者のメールアドレスの制約に従う引数を受け取った場合、値オブジェクトを生成できる', () => {
-    it.todo('"shimoke4869@gmail.com" を引数に指定し、値オブジェクトを生成する');
+    it('"shimoke4869@gmail.com" を引数に指定し、値オブジェクトを生成する', () => {
+      // Arrange
+      const expected = 'shimoke4869@gmail.com';
+
+      // Act
+      const actual = ParticipantEmail.create(expected);
+
+      // Assert
+      expect(actual.value).toBe(expected);
+    });
   });
   describe('参加者のメールアドレスの制約に従わない引数を受け取った場合、例外を送出する', () => {
-    it.todo(
-      '"shimokawa" を引数に指定し、"メールアドレスが正しいフォーマットではありません。" というメッセージの例外を送出する',
-    );
+    it('"shimokawa" を引数に指定し、"メールアドレスが正しいフォーマットではありません。" という例外を送出する', () => {
+      // Arrange
+      const expected = 'メールアドレスが正しいフォーマットではありません。';
+      const testData = 'shimokawa';
+
+      // Act & Assert
+      expect(ParticipantEmail.create(testData)).toThrowError(expected);
+    });
   });
 });
