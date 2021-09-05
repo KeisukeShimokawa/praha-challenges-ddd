e3ec18375149c1991e173daee6b3f860a609a6b1
KeisukeShimokawa <shimoke4869@gmail.com>
2021-09-05 16:37:37 +0900

    参加者名の値オブジェクトを追加 #1

---
+++ src/domain/participants/ParticipantName.ts
@@ -0,0 +1,15 @@
+import { ValueObject } from '../shared/ValueObject';
+
+export class ParticipantName extends ValueObject<string, 'ParticipantName'> {
+  private constructor(name: string) {
+    super(name);
+  }
+
+  public static create(name: string): ParticipantName {
+    if (!name) {
+      throw new Error('名前が指定されていません。');
+    }
+
+    return new ParticipantName(name);
+  }
+}
