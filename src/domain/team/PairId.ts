77fd1c41af6d24ed1a9f7060bbd44e44faacd869
KeisukeShimokawa <shimoke4869@gmail.com>
2021-09-05 17:04:02 +0900

    ペアに関するドメインを追加 #1

--- src/domain/team/PairId.ts
+++ src/domain/team/PairId.ts
@@ -1,6 +1,6 @@
 import { Identifier } from '../shared/Identifier';
 
-export class PairId extends Identifier<PairId> {
+export class PairId extends Identifier<'PairId'> {
   constructor(id?: string) {
     super(id);
   }
