fc382a7315c1137d0260a6a9bd6d65b16f6fe02c
KeisukeShimokawa <shimoke4869@gmail.com>
2021-09-11 18:49:15 +0900

    feat: E2Eテスト用のデータを生成するためのFactoryメソッドを借り追加 #1

---
+++ test/utils/TestFactory.ts
@@ -0,0 +1,17 @@
+import { EnrollmentStatusType } from 'src/domain/participant/vo/EnrollmentStatus';
+import { PrismaService } from 'src/shared/prisma/PrismaService';
+
+const client = new PrismaService();
+
+const creator = async () => {
+  await client.participant.createMany({
+    data: [
+      {
+        id: '1',
+        name: '1',
+        email: '1',
+        enrollmentStatus: EnrollmentStatusType.ENROLLMENT,
+      },
+    ],
+  });
+};
