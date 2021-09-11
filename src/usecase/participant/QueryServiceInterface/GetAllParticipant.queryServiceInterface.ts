2dd59ec0479562a22b0c33a930fde893a04e433f
KeisukeShimokawa <shimoke4869@gmail.com>
2021-09-11 18:43:01 +0900

    feat: クライアントに在籍ステータスも返すように、プロパティを追加 #1

--- src/usecase/participant/QueryServiceInterface/GetAllParticipant.queryServiceInterface.ts
+++ src/usecase/participant/QueryServiceInterface/GetAllParticipant.queryServiceInterface.ts
@@ -2,6 +2,7 @@ export class GetAllParticipantDTO {
   public readonly id: string;
   public readonly name: string;
   public readonly email: string;
+  public readonly enrollmentStatus: string;
   public readonly tasks: string[];
   public readonly pair: string;
 
@@ -9,13 +10,15 @@ export class GetAllParticipantDTO {
     id: string;
     name: string;
     email: string;
+    enrollmentStatus: string;
     tasks: string[];
     pair: string;
   }) {
-    const { id, name, email, tasks, pair } = props;
+    const { id, name, email, enrollmentStatus, tasks, pair } = props;
     this.id = id;
     this.name = name;
     this.email = email;
+    this.enrollmentStatus = enrollmentStatus;
     this.tasks = tasks;
     this.pair = pair;
   }
