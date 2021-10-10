376b8ef20c077d209ec337202615033e6d4b1609
KeisukeShimokawa <shimoke4869@gmail.com>
2021-10-10 17:41:03 +0900

    feat: 全チームの情報を取得するための機能の依存関係を追加 #3

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,26 @@
 import { Module } from '@nestjs/common';
 import { GetAllParticipantQueryService } from './infrastructure/db/participant/QueryService/GetAllParticipant.QueryService';
 import { GetAllTaskQueryService } from './infrastructure/db/task/QueryService/GetAllTask.QueryService';
+import { GetAllTeamQueryService } from './infrastructure/db/team/GetAllTeam.QueryService';
 import { ParticipantController } from './presentation/participant/participant.controller';
 import { TaskController } from './presentation/task/task.controller';
+import { TeamController } from './presentation/team/team.controller';
 import { PrismaService } from './shared/prisma/PrismaService';
 import { GetAllParticipantUseCase } from './usecase/participant/GetAllParticipant.usecase';
 import { GetAllTaskUseCase } from './usecase/task/GetAllTask.usecase';
+import { GetAllTeamUseCase } from './usecase/team/GetAllTeam.usecase';
 
 @Module({
   imports: [],
-  controllers: [ParticipantController, TaskController],
+  controllers: [ParticipantController, TaskController, TeamController],
   providers: [
     PrismaService,
     GetAllParticipantUseCase,
     GetAllParticipantQueryService,
     GetAllTaskUseCase,
     GetAllTaskQueryService,
+    GetAllTeamUseCase,
+    GetAllTeamQueryService,
     {
       provide: 'IGetAllParticipantQueryService',
       useClass: GetAllParticipantQueryService,
@@ -23,6 +28,10 @@ import { GetAllTaskUseCase } from './usecase/task/GetAllTask.usecase';
     {
       provide: 'IGetAllTaskQueryService',
       useClass: GetAllTaskQueryService,
+    },
+    {
+      provide: 'IGetAllTeamQueryService',
+      useClass: GetAllTeamQueryService,
     },
   ],
 })
