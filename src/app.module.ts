8df460d203d2eb26691370a7742fcc50c8ce6867
KeisukeShimokawa <shimoke4869@gmail.com>
2021-10-17 16:03:48 +0900

    チームIDを取得する機能の依存関係を登録

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { GetAllParticipantQueryService } from './infrastructure/db/participant/QueryService/GetAllParticipant.QueryService';
 import { GetAllTaskQueryService } from './infrastructure/db/task/QueryService/GetAllTask.QueryService';
 import { GetAllTeamQueryService } from './infrastructure/db/team/GetAllTeam.QueryService';
+import { GetTeamByIdQueryService } from './infrastructure/db/team/GetTeamById.QueryService';
 import { ParticipantController } from './presentation/participant/participant.controller';
 import { TaskController } from './presentation/task/task.controller';
 import { TeamController } from './presentation/team/team.controller';
@@ -9,6 +10,7 @@ import { PrismaService } from './shared/prisma/PrismaService';
 import { GetAllParticipantUseCase } from './usecase/participant/GetAllParticipant.usecase';
 import { GetAllTaskUseCase } from './usecase/task/GetAllTask.usecase';
 import { GetAllTeamUseCase } from './usecase/team/GetAllTeam.usecase';
+import { GetTeamByIdUseCase } from './usecase/team/GetTeamById.usecase';
 
 @Module({
   imports: [],
@@ -21,6 +23,8 @@ import { GetAllTeamUseCase } from './usecase/team/GetAllTeam.usecase';
     GetAllTaskQueryService,
     GetAllTeamUseCase,
     GetAllTeamQueryService,
+    GetTeamByIdUseCase,
+    GetTeamByIdQueryService,
     {
       provide: 'IGetAllParticipantQueryService',
       useClass: GetAllParticipantQueryService,
@@ -32,6 +36,10 @@ import { GetAllTeamUseCase } from './usecase/team/GetAllTeam.usecase';
     {
       provide: 'IGetAllTeamQueryService',
       useClass: GetAllTeamQueryService,
+    },
+    {
+      provide: 'IGetTeamByIdQueryService',
+      useClass: GetTeamByIdQueryService,
     },
   ],
 })
