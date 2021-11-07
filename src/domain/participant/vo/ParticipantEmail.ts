94d06487f506eac3d9f0e4ab19671f1b2590ebd8
KeisukeShimokawa <shimoke4869@gmail.com>
2021-11-08 00:25:51 +0900

    feat: メールアドレスを検証するための値オブジェクトを修正

--- src/domain/participant/vo/ParticipantEmail.ts
+++ src/domain/participant/vo/ParticipantEmail.ts
@@ -1,8 +1,9 @@
 // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
 
+import { DomainException } from '../../shared/DomainException';
 import { ValueObject } from '../../shared/ValueObject';
 
-export class ParticipantEmail extends ValueObject<string, 'ParticipantEmail'> {
+export class ParticipantEmail extends ValueObject<string> {
   private static isValidEmail(email: string): boolean {
     const re =
       /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
@@ -11,11 +12,21 @@ export class ParticipantEmail extends ValueObject<string, 'ParticipantEmail'> {
     return result;
   }
 
+  /**
+   * メールアドレスを生成するメソッド
+   *
+   * @param email メールアドレス
+   * @returns フォーマットが有効であり、小文字に変換したメールアドレス
+   */
   public static create(email: string): ParticipantEmail {
-    if (!this.isValidEmail(email)) {
-      throw new Error('メールアドレスが正しいフォーマットではありません。');
+    const emailLowerCase = email.toLowerCase();
+
+    if (!this.isValidEmail(emailLowerCase)) {
+      throw new DomainException(
+        'メールアドレスが正しいフォーマットではありません。',
+      );
     }
 
-    return new ParticipantEmail(email.toLowerCase());
+    return new ParticipantEmail(emailLowerCase);
   }
 }
