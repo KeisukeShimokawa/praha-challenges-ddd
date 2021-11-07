aca087b3bd2fe997bb8f0aa3f6a9c2614a98f6d4
KeisukeShimokawa <shimoke4869@gmail.com>
2021-11-08 01:15:39 +0900

    feat: 不要な課題タイトルは一旦削除

--- src/domain/task/vo/TaskDescription.ts
+++
@@ -1,7 +0,0 @@
-import { ValueObject } from 'src/domain/shared/ValueObject';
-
-export class TaskDescription extends ValueObject<string, 'TaskDescription'> {
-  public static create(descriptopn: string): TaskDescription {
-    return new TaskDescription(descriptopn);
-  }
-}
