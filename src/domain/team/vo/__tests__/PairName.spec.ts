14d0b424306700f477d63382eb9acacba1093168
shimopino <shimoke4869@gmail.com>
2021-11-22 19:58:52 +0900

    feat: ペア名のテストケースにParameterized Testを導入

--- src/domain/team/vo/__tests__/PairName.spec.ts
+++ src/domain/team/vo/__tests__/PairName.spec.ts
@@ -16,30 +16,21 @@ describe('値オブジェクト<"ペア名"> PairName', () => {
   });
 
   describe('ペア名に関する制約を破る引数を受け取った場合、ドメイン層固有の例外が送出される', () => {
-    it('"12" を引数に指定し、"ペア名が正しいフォーマットではありません。" という例外を送出する', () => {
-      // Arrange
-      const expected = 'ペア名が正しいフォーマットではありません。';
-      const testData = '12';
+    it.each`
+      name    | expected
+      ${''}   | ${'ペア名が正しいフォーマットではありません。'}
+      ${'12'} | ${'ペア名が正しいフォーマットではありません。'}
+      ${'ab'} | ${'ペア名が正しいフォーマットではありません。'}
+    `(
+      '"$name" を引数に指定し、"$expected" という例外を送出する。',
+      ({ name, expected }) => {
+        // Act
+        const target = () => PairName.create(name);
 
-      // Act
-      const target = () => PairName.create(testData);
-
-      // Assert
-      expect(target).toThrow(expected);
-      expect(target).toThrow(DomainException);
-    });
-
-    it('"" を引数に指定し、"ペア名が正しいフォーマットではありません。" という例外を送出する', () => {
-      // Arrange
-      const expected = 'ペア名が正しいフォーマットではありません。';
-      const testData = '';
-
-      // Act
-      const target = () => PairName.create(testData);
-
-      // Assert
-      expect(target).toThrow(expected);
-      expect(target).toThrow(DomainException);
-    });
+        // expect
+        expect(target).toThrow(expected);
+        expect(target).toThrow(DomainException);
+      },
+    );
   });
 });
